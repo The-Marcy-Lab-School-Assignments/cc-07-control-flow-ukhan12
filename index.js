9ce8297252f23d1061ad309aa5524c407048c832
uzma <ubkhan3@gmail.com>
2021-10-13 14:18:44 +0000

    complete all functions

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
 //1
-function sumOfFourAndSix(){
-  
+function sumOfFourAndSix() {
+    let sum = 0;
+    for (let i = 0; i <= 1000; i++) {
+        if (i % 4 == 0 && i % 6 == 0) {
+            sum += i;
+        }
+    }
+    return sum;
 }
 
 //2
-function oddAndEvenInFifteen(){
-  
+function oddAndEvenInFifteen() {
+    for (let i = 0; i <= 15; i++) {
+        if (i % 2 == 0) {
+            console.log(i + " is even");
+        }
+        else
+            console.log(i + " is odd");
+    }
 }
 
 //3
-function assignGrade(){
-  
-}
+function assignGrade(score) {
+    if (score >= 0 && score <= 100) {
+        if (score >= 91) {
+            return 'A';
+        }
+        else if (score >= 81) {
+                return 'B';
+            }
+            else if (score >= 71) {
+                    return 'C';
+                }
+                else if (score >= 65) {
+                    return 'D';
+                }
+                else  {
+                    return 'F';
+                }
+            }
+        }
 
 
-
-
-
+//console.log(sumOfFourAndSix());
+//oddAndEvenInFifteen();
+console.log(assignGrade(101));
 
